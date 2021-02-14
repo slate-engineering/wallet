58e72cbd27d4883aaf5ed665cad4d4516e734f54
jimmylee <send@jimmy.email>
2021-02-13 20:11:13 -0800

    adds onboarding button

--- src/scenes/ScenePortfolio.js
+++ src/scenes/ScenePortfolio.js
@@ -21,6 +21,8 @@ export default class ScenePortfolio extends React.Component {
       transactions = [...a.transactions, ...transactions];
     }
 
+    const hasAddresses = this.props.accounts.addresses && !!this.props.accounts.addresses.length;
+
     console.log(balance);
     console.log(transactions);
 
@@ -33,6 +35,14 @@ export default class ScenePortfolio extends React.Component {
             {this.props.accounts.addresses.length} addresses and {transactions.length} transactions.
           </p>
 
+          {!hasAddresses ? (
+            <div className="body-card" onClick={() => this.props.onNavigate("ADD_ADDRESS")}>
+              <h2 className="body-heading-two">Add or create your address</h2>
+              <p className="body-paragraph">
+                To start using this wallet. Add or create your address.
+              </p>
+            </div>
+          ) : null}
           {balance > 0 ? (
             <React.Fragment>
               <h2 className="body-heading" style={{ marginTop: 24 }}>
@@ -41,7 +51,6 @@ export default class ScenePortfolio extends React.Component {
               <p className="body-paragraph">Balance</p>
             </React.Fragment>
           ) : null}
-
           {transactions.length ? (
             <React.Fragment>
               <h2 className="body-heading-two" style={{ marginTop: 24 }}>
